66c82c3650963345bfc8d9ac39c9862c28bd0003
Gabriel <gomes.g.r@outlook.com>
2020-03-04 11:52:28 -0300

    new file:   .formatter.exs 	new file:   .gitignore 	modified:   README.md 	new file:   lib/cards.ex 	new file:   meu_deck 	new file:   mix.exs 	new file:   mix.lock 	new file:   test/cards_test.exs 	new file:   test/test_helper.exs

---
+++ lib/cards.ex
@@ -0,0 +1,90 @@
+defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of cards
+
+  ## Example
+
+      iex> Cards.create_deck()
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",  
+       "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+       "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+       "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+       "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+
+  """
+  def create_deck() do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # List comprehension, kinda map
+    # Number 1 solution
+    # cards = for suit <- suits do
+    #   for value <- values do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    # Number 2 solution
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    the `hand-size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write!(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do 
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, reason} -> "Does not exist: #{reason}"
+    end
+  end
+
+  def create_hand(hand_size) do
+    create_deck()
+    |> shuffle()
+    |> deal(hand_size)
+  end
+end
